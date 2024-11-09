9bbd5d9546755e22d13b4be33f7522a6aa3179c8
itc-s22020 <s22020@std.it-college.ac.jp>
2024-11-09 17:57:26 +0900

    各ページの作成

---
+++ lib/page/hotel.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HotelPage extends StatelessWidget {
+  const HotelPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(child: Text("#HotelPage"))
+    );
+  }
+}
---
+++ lib/page/route.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class RoutePage extends StatelessWidget {
+  const RoutePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(child: Text("#RoutePage"))
+    );
+  }
+}
